8335ee4cef3c2776c504fcfd821cd8797c78d165
hackydh <dhgacky@gmail.com>
2018-10-31 00:00:58 +0800

    add block_queue

---
+++ block_queue/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+#include "block_queue.h"
+
+int main(){
+    utils::BlockQueue<int> bq(5);
+    std::thread t([&]{
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        bq.put(5);
+    });
+    if(!bq.poll()){
+        std::cout<<*bq.take()<<std::endl;
+    }
+    t.join();
+    return 0;
+}
