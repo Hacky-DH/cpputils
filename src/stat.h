946f68ad8c762dc02c16776b79a1c75cdac0459e
DHacky <dhgacky@gmail.com>
2022-12-14 14:34:27 +0800

    Create stat.h
    GetRSS

---
+++ src/stat.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+
+namespace utils {
+
+class Stat {
+ public:
+  static int64_t GetRSS() {
+    std::fstream stat("/proc/self/stat");
+    if (!stat.is_open()) {
+      return 0;
+    }
+    std::string str;
+    int64_t size;
+    // i=22 VSZ Byte
+    for (size_t i = 0; i < 25; ++i) {
+      if (i == 23) {
+        // i=23, rss is page size
+        stat >> size;
+      } else {
+        stat >> str;
+      }
+    }
+    return size * 4;
+  }
+
+  static std::string GetRSSStr() {
+    int64_t rss = GetRSS();
+    double result = 1.;
+    std::string fotmat("%.3f");
+    char buff[64];
+    if (rss >= (1ULL << 30)) {
+      fotmat.append("TiB");
+      result = (1ULL << 30);
+    } else if (rss >= (1ULL << 20)) {
+      fotmat.append("GiB");
+      result = (1ULL << 20);
+    } else if (rss >= (1ULL << 10)) {
+      fotmat.append("MiB");
+      result = (1ULL << 10);
+    } else {
+      fotmat.append("KiB");
+      result = 1.;
+    }
+    snprintf(buff, 64, fotmat.c_str(), rss / result);
+    return buff;
+  }
+};
+}  // namespace utils
